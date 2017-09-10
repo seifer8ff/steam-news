a8545eadb265cdb043c3c7ab74366e5eeb8fee09
seifer8ff <brady.bcmp@gmail.com>
2017-09-10 13:38:16 -0700

    Footer and header styling + fixes to click outside + mobile styles
    Click outside now detect touches (for mobile) and prevents scrolling.
    Mobile now disallows horizontal scrolling.
    Header now fits login button on mobile.
    Footer is slightly better positioned, but still needs work.

--- src/app/click-outside.directive.ts
+++ src/app/click-outside.directive.ts
@@ -9,11 +9,19 @@ export class ClickOutsideDirective {
 
   @Output() public appClickOutside = new EventEmitter();
 
-    @HostListener('document:click', ['$event.target']) public onClick(targetElement) {
-      const clickedInside = this.elementRef.nativeElement.contains(targetElement) || targetElement.classList.contains('click-outside');
+    @HostListener('document:click', ['$event'])
+    @HostListener('document:touchend', ['$event'])
+    handleClick(event) {
+      console.log('clicked or touched');
+      console.log(event.target);
+      const clickedInside = this.elementRef.nativeElement.contains(event.target) || event.target.classList.contains('click-outside');
       if (!clickedInside) {
           this.appClickOutside.emit(null);
+          event.preventDefault();
       }
     }
 
+    @HostListener('document:touchmove', ['$event']) handleScroll(event) {
+      event.preventDefault();
+    }
 }
