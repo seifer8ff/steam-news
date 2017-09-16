08ccf4b84af4da684187a2b6b9de6c122235f041
seifer8ff <brady.bcmp@gmail.com>
2017-09-15 21:26:13 -0700

    Added scroll to top on click header
    Scroll to top wasn’t working on iOS, so added custom method to do it by
    clicking on header.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 import { StateService } from '../user/../state.service';
 import { UserService } from '../user/user.service';
@@ -10,15 +10,26 @@ import { UserService } from '../user/user.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public userService: UserService, public stateService: StateService) { }
+  scrollWindow: HTMLElement;
+
+
+  constructor(public userService: UserService, public stateService: StateService, private elementRef: ElementRef) { }
 
   ngOnInit() {
+    this.scrollWindow = <HTMLElement>document.querySelector(".content");
   }
 
   openDropdown(event) {
     if (window.innerWidth <= 768 && event.target.firstElementChild) {
       event.target.classList.toggle("hover");
       event.target.firstElementChild.classList.toggle("open");
+    }
+  }
+
+  handleTouch(event) {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target) && !event.target.classList.contains("header-item");
+    if (clickedInside) {
+        this.scrollWindow.scrollTop = 0;
     }
   }
 
